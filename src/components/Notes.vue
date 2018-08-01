3858472542d0a6a0e0f2f3109ec5496eb9e47c25
baoyuan <1045149141@qq.com>
2018-08-01 10:58:36 +0800

    111

---
+++ src/components/Notes.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h4>Vue Notes App</h4>
+    <note></note>
+  </div>
+</template>
+
+<script>
+import Note from './Note'
+export default {
+  components: {
+    Note
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 项目入口文件
+// 导入模块
 import Vue from 'vue'
+// 导入同级目录下面的App.vue组件
 import App from './App'
 
 Vue.config.productionTip = false
